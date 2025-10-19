04916efe69dfca0ce08200345ff747ac9ab73868
nakylee-crtr <nakylee@gmail.com>
2025-10-19 11:55:46 +0800

    Calculate result when input null

---
+++ src/main/java/calculator/Calculation.java
@@ -0,0 +1,20 @@
+package calculator;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Calculation {
+    public static int calculate(String userInput, String delimiter, String delimPattern) {
+
+        int result = 0;
+
+        // 빈 문자열
+        if (userInput == null || userInput.isEmpty()) {
+            result += 0;
+        }
+
+
+        return result;
+
+    }
+}
